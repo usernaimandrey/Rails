067b9b9e1ac13a9889d48dc521757bb40cff05fe
@hexlet/cli <support@hexlet.io>
2022-07-08 20:32:53 +0000

    @hexlet/cli: submit

---
+++ exercises/templates/config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :tasks
+  root 'home#index'
+  # BEGIN
+  resources :tasks
+  # END
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
