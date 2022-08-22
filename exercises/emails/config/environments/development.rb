1902b2d35dc890040a826728eb68cc36b05120bf
@hexlet/cli <support@hexlet.io>
2022-08-22 16:00:23 +0300

    @hexlet/cli: submit

--- exercises/emails/config/environments/development.rb
+++ exercises/emails/config/environments/development.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
   # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
-  #   :tls => true,
+  #   tls: true,
   #   address: 'smtp.yandex.ru',
   #   port: 465,
   #   domain: 'yandex.ru',
--- exercises/emails/config/environments/production.rb
+++ exercises/emails/config/environments/production.rb
@@ -68,11 +68,11 @@ Rails.application.configure do
     user_name: ENV.fetch('MAIL_USERNAME', nil),
     password: ENV.fetch('MAIL_PASSWORD', nil),
     address: ENV.fetch('MAIL_HOST', nil),
-    domain: ENV.fetch('MAIL_HOST_Y', nil),
+    domain: ENV.fetch('MAIL_HOST', nil),
     port: ENV.fetch('SMTP_PORT', '25'),
-    # authentication: :cram_md5,
-    authentication: 'plain',
-    tls: true
+    authentication: :cram_md5
+    # authentication: 'plain',
+    # tls: true
   }
   # END
 
