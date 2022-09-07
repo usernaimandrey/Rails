c504eeec940a8888238715c77ca330b6fbb8e2cd
@hexlet/cli <support@hexlet.io>
2022-09-07 21:42:04 +0300

    @hexlet/cli: submit

--- exercises/api/app/controllers/api/users_controller.rb
+++ exercises/api/app/controllers/api/users_controller.rb
@@ -4,15 +4,15 @@ module Api
   class UsersController < Api::ApplicationController
     # BEGIN
     def index
-      @users = User.all
+      users = User.all
 
-      respond_with @users.as_json(only: %i[first_name last_name email addres state])
+      respond_with users.as_json(only: %i[first_name last_name email addres state])
     end
 
     def show
-      @user = User.find(params[:id])
+      user = User.find(params[:id])
 
-      respond_with @user.as_json(only: %i[first_name last_name email addres state])
+      respond_with user.as_json(only: %i[first_name last_name email addres state])
     end
     # END
   end
